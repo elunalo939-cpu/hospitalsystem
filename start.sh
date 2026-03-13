dacb3d5c6cfac41234dc948318f964d1b7e15e7b
elunalo939-cpu <elunalo939@gmail.com>
2026-03-13 10:06:01 +0300

    Remove database migration from start.sh
    Removed database migration command from start script.

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 set -e
-
-# Run database migrations
-mysql -h "$MYSQL_HOST" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DB" < create_db.sql
-
-# Start PHP server on Railway's assigned port
 php -S 0.0.0.0:$PORT
